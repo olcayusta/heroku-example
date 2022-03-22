f478d0bbd3428b30e75649c02ea50d51e56d174c
olcay <olcayusta02@gmail.com>
2022-03-22 16:15:51 +0300

    add socket.io

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 import express from 'express'
+import {Server} from 'socket.io'
 
 const app = express()
 
 const PORT = process.env.PORT || 5000
+
+const io = new Server(6000)
+
+io.on('connection', (socket) => {
+    io.emit('welcome', 'hi')
+
+    socket.on('chat message', (msg) => {
+        io.emit('hello', 'hi')
+    })
+})
 
 app.get('/', async (req, res) => {
     res.send('Merhaba dünya!')
