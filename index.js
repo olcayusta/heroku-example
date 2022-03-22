f062d862f9d29dba444541edd80929da65e7a8ee
olcay <olcayusta02@gmail.com>
2022-03-22 05:49:11 +0300

    update

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import express from 'express'
 
 const app = express()
 
+const PORT = process.env.PORT || 5000
+
 app.get('/', async (req, res) => {
     res.send('Merhaba dünya!')
 })
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(PORT, () => {
     console.log('Uygulama 3000 portundan ayağa kalktı...')
 })
