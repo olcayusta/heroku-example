4642cadcf0392bcaed4df84293b17a39218179b8
olcay <olcayusta02@gmail.com>
2022-03-22 16:30:57 +0300

    added socket.io example

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
 import express from 'express'
 import {Server} from 'socket.io'
+import {fileURLToPath} from 'url'
+import {dirname} from 'path'
 
 const app = express()
 
+import {createServer} from 'http'
+const server = createServer(app);
+
 const PORT = process.env.PORT || 5000
 
-const io = new Server(6000)
+const io = new Server(server)
 
 io.on('connection', (socket) => {
-    io.emit('welcome', 'hi')
+    socket.broadcast.emit('hi');
 
     socket.on('chat message', (msg) => {
-        io.emit('hello', 'hi')
-    })
+        io.emit('chat message', msg);
+    });
 })
 
 app.get('/', async (req, res) => {
-    res.send('Merhaba dünya!')
+    const __dirname = dirname(fileURLToPath(import.meta.url))
+    res.sendFile(__dirname + '/index.html')
 })
 
 app.get('/hello', async (req, res) => {
     res.send('Hello')
 })
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log('Uygulama 3000 portundan ayağa kalktı...')
 })
